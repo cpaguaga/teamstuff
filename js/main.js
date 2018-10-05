a2904bf6394ab0c9266468c43dd079a7d56f5189
Jaclyn Flomen <JaclynFlomen@Jaclyns-MacBook-Pro.local>
2018-10-05 17:36:02 -0400

    working on lightbox

--- js/main.js
+++ js/main.js
@@ -1,40 +1,37 @@
-// Open the Modal
-function openModal() {
-  document.getElementById('myModal').style.display = "block";
+(function() {
+	"use strict";
+	console.log("SEAF fired");
+
+var modal = document.querySelector('lightbox');
+var btn = document.querySelector("lightbox_click");
+var span = document.querySelector("close")[0];
+
+function block_display()
+{
+	console.log()
+  btn.onclick = function(block_display) {
+  modal.style.display = "block";
+}
 }
 
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+function closeone()
+{
+  console.log(closeone)
+span.onclick = function() {
+    modal.style.display = "none";
+}
 }
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function display_back()
+{
+  console.log(display_back)
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+modal.addEventListener("click", block_display, false);
+span.addEventListener("click", closeone, false);
+window.addEventListener("click", display_back, false);
