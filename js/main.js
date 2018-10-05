553581f84dbd9ae812bcc6d2045fddc832f23dd0
jaclynflomen <j_floman@fanshaweonline.ca>
2018-10-05 18:58:45 -0400

    more css

--- js/main.js
+++ js/main.js
@@ -2,36 +2,16 @@
 	"use strict";
 	console.log("SEAF fired");
 
-var modal = document.querySelector('lightbox');
-var btn = document.querySelector("lightbox_click");
-var span = document.querySelector("close")[0];
 
+var lp = document.querySelector('lightboxPics');
+var body = document.querySelector('body')
 function block_display()
 {
 	console.log()
-  btn.onclick = function(block_display) {
-  modal.style.height = "100%";
+  lp.onclick = function(block_display) {
+  lp.style.height = "100%";
+	body.style.opacity = "25%";
 }
 }
 
-function closeone()
-{
-  console.log(closeone)
-span.onclick = function() {
-    modal.style.display = "none";
-}
-}
-
-function display_back()
-{
-  console.log(display_back)
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-}
-
-modal.addEventListener("click", block_display, false);
-span.addEventListener("click", closeone, false);
-window.addEventListener("click", display_back, false);
+lp.addEventListener("click", block_display, false);
