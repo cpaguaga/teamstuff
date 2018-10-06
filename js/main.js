6bbdf0d216d40f9f6d2bbe3302cf769a8b8abed6
jaclynflomen <j_floman@fanshaweonline.ca>
2018-10-05 22:58:49 -0400

    nav colour changes

--- js/main.js
+++ js/main.js
@@ -3,26 +3,14 @@
 	console.log("SEAF fired");
 
 
-var lp = document.getElementsByClassName('lightboxPics');
-var body = document.querySelector('body');
-var blockdisplay = document.getElementsByClassName('lightbox');
-
-function (blockdisplay){
-	console.log()
-  lp.style.height = "100%";
-	body.style.opacity = "25%";
-}
-
-
-lp.addEventListener("click", block_display, false);
-})
-
-
-var navT = document.getElementsByClassName("topnav");
+var navT = document.querySelector(".topnav");
+var buttonX = document.querySelector("#menuButton")
 
 	function (toggleNav){
-		console.log(toggleNav)
+		console.log();
 	  navT.style.display = "block";
 	}
 
-	navT.addEventListener("click", toggleNav, false);
+	buttonX.addEventListener("click", toggleNav, false);
+
+}());
