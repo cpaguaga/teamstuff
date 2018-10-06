cbadbcf84c168f8c6bf942d889b1711bec83cc73
cpaguaga <31866586+cpaguaga@users.noreply.github.com>
2018-10-05 23:12:26 -0400

    troubleshooting lightbox

--- js/main.js
+++ js/main.js
@@ -2,15 +2,22 @@
 	"use strict";
 	console.log("SEAF fired");
 
-
 var lp = document.getElementsByClassName('lightboxPics');
 var body = document.querySelector('body');
 var blockdisplay = document.getElementsByClassName('lightbox');
 
-function (blockdisplay){
+function blockdisplay(){
 	console.log()
   lp.style.height = "100%";
 	body.style.opacity = "25%";
+}
+
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
 }
 
 
